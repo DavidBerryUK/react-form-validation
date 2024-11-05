c53ec16d80a2229f477ad5663edadaefb046e4d3
DavidBerryUK <DavidBerryUK@hotmail.co.uk>
2024-11-05 18:09:53 +0000

    converted RSVP to use better schema. need to amend all models and base class.

--- src/viewModels/RsvpFormViewModel.ts
+++ src/viewModels/RsvpFormViewModel.ts
@@ -1,28 +1,27 @@
-import { Map } from "immutable";
 import EnumFieldDataType from "../library/packageViewModelp/enums/EnumFieldDataType";
-import FieldModel, { FieldSchema, FieldTypeString } from "../library/packageViewModelp/base/FieldModel";
+import FieldModel from "../library/packageViewModelp/base/FieldModel";
 import RuleInteger from "../library/packageViewModelp/validation/rules/RuleInteger";
 import RuleMandatory from "../library/packageViewModelp/validation/rules/RuleMandatory";
 import RuleMaxLength from "../library/packageViewModelp/validation/rules/RuleMaxLength";
 import RuleMinLength from "../library/packageViewModelp/validation/rules/RuleMinLength";
 import RuleValueBetween from "../library/packageViewModelp/validation/rules/RuleValueBetween";
-import ViewModelBase, { FormSchemaInitialise } from "../library/packageViewModelp/base/BaseViewModel";
+import ViewModelBase from "../library/packageViewModelp/base/BaseViewModel";
+import FieldSchema from "../library/packageViewModelp/base/FieldSchema";
+import ViewModelSchema, { SchemaBase } from "../library/packageViewModelp/base/ViewModelSchema";
+import { Map } from "immutable";
 
-type RsvpSchema = {
-  guestName: FieldSchema;
-  attending: FieldSchema;
-  numberOfGuests: FieldSchema;
-};
+class RsvpViewModelSchema extends ViewModelSchema {
+  fields: SchemaBase = {
+    guestName: new FieldSchema("Guest Name", EnumFieldDataType.string, [new RuleMandatory(), new RuleMinLength(2), new RuleMaxLength(100)]),
+    attending: new FieldSchema("Attending", EnumFieldDataType.boolean),
+    numberOfGuests: new FieldSchema("Number Of Guests (1-4)", EnumFieldDataType.number, [new RuleMandatory(), new RuleInteger(), new RuleValueBetween(1, 4)]),
+  };
 
-const schemaConfig: FormSchemaInitialise = {
-  guestName: { caption: "Guest Name", dataType: EnumFieldDataType.string, rules: [new RuleMandatory(), new RuleMinLength(2), new RuleMaxLength(100)] },
-  attending: { caption: "Attending?", dataType: EnumFieldDataType.boolean, rules: [] },
-  numberOfGuests: {
-    caption: "Number Of Guests (1-4)",
-    dataType: EnumFieldDataType.number,
-    rules: [new RuleMandatory(), new RuleInteger(), new RuleValueBetween(1, 4)],
-  },
-};
+  constructor() {
+    super();
+    this.populateFieldNames();
+  }
+}
 
 /**
  * The Contact Form is imutable, meaning that it is read only and any
@@ -32,7 +31,8 @@ const schemaConfig: FormSchemaInitialise = {
  */
 export class RsvpFormViewModel extends ViewModelBase {
   // the schema provides field meta data such as fieldname, ui-caption, data types
-  private static readonly schema: RsvpSchema = this.createSchemaFromConfig<RsvpSchema>(schemaConfig);
+
+  static modelSchema = new RsvpViewModelSchema();
 
   /****************************************************/
   /* Initialize RsvpFormViewModel with Field Models */
@@ -44,22 +44,35 @@ export class RsvpFormViewModel extends ViewModelBase {
    * @param numberOfGuests - Initial value for the "numberOfGuests" field.
    * @returns {RsvpFormViewModel} A new instance of RsvpFormViewModel with initialized fields.
    */
-  static CreateViewModel(guestName: FieldTypeString, attending: FieldTypeString, numberOfGuests: FieldTypeString): RsvpFormViewModel {
+  static createViewModel(guestName: string, attending: boolean, numberOfGuests: number): RsvpFormViewModel {
     const initialValues = { guestName, attending, numberOfGuests };
 
-    // Map field names to FieldModel instances based on the schema and initial values
-    const fields = Map<string, FieldModel>(
-      Object.keys(RsvpFormViewModel.schema).map((key) => {
-        const fieldSchema = RsvpFormViewModel.schema[key as keyof RsvpSchema];
-        const fieldValue = initialValues[key as keyof RsvpSchema];
-        return [fieldSchema.fieldName, FieldModel.fromSchema(fieldSchema, fieldValue)];
-      }),
-    );
+    let fields = Map<string, FieldModel>();
+
+    // Define the type for keys of initialValues
+    type InitialValueKeys = keyof typeof initialValues;
+
+    // Get the keys of the RsvpFormViewModel's fields schema
+    const rsvpFieldKeys = Object.keys(RsvpFormViewModel.modelSchema.fields) as Array<keyof typeof RsvpFormViewModel.modelSchema.fields>;
+
+    rsvpFieldKeys.forEach((key) => {
+      const fieldKey = key; // No need for type assertion, already a keyof
+
+      const fieldSchema = RsvpFormViewModel.modelSchema.fields[fieldKey];
+
+      // Ensure the key exists in initialValues
+      if (key in initialValues) {
+        const fieldValue = initialValues[key as InitialValueKeys]; // Now we can safely access it
+        const field = FieldModel.fromSchema(fieldSchema, fieldValue);
+        fields = fields.set(field.fieldName, field);
+      }
+    });
+
     return new RsvpFormViewModel(fields);
   }
 
   static CreateEmptyViewModel(): RsvpFormViewModel {
-    return this.CreateViewModel("", "", "");
+    return this.createViewModel("", false, 0);
   }
 
   /****************************************************/
@@ -67,22 +80,22 @@ export class RsvpFormViewModel extends ViewModelBase {
   /****************************************************/
 
   get guestName(): FieldModel {
-    return this.fields.get(RsvpFormViewModel.schema.guestName.fieldName)!;
+    return this.fields.get(RsvpFormViewModel.modelSchema.fields.guestName.fieldName)!;
   }
 
   get attending(): FieldModel {
-    return this.fields.get(RsvpFormViewModel.schema.attending.fieldName)!;
+    return this.fields.get(RsvpFormViewModel.modelSchema.fields.attending.fieldName)!;
   }
 
   get numberOfGuests(): FieldModel {
-    return this.fields.get(RsvpFormViewModel.schema.numberOfGuests.fieldName)!;
+    return this.fields.get(RsvpFormViewModel.modelSchema.fields.numberOfGuests.fieldName)!;
   }
 
   /****************************************************/
   /* Events                                           */
   /****************************************************/
   onFieldUpdated(oldField: FieldModel, newField: FieldModel): void {
-    if (newField.fieldName === RsvpFormViewModel.schema.attending.fieldName) {
+    if (newField.fieldName === RsvpFormViewModel.modelSchema.fields.attending.fieldName) {
       console.log(`Attending Status has changed to [${newField.value?.toLocaleString()}]`);
     }
   }
